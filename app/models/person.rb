aa5d58dfa680c5ee488a3d75b4b7db772a7ecae6
TakayukiIshibashi <tk.takayuki.ishibashi@gmail.com>
2024-03-24 18:55:41 +0900

    meaningless

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,5 @@
 class Person < ApplicationRecord
     validates :name, presence: true
     validates :age, numericality: {only_integer:true,less_than:125}
-    validates :mail, uniqueness:true ,presence: true
-    
+    validates :mail, email:true
 end
